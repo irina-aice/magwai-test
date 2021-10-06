14ca7249414f5f8881b17ab2084bb39b7147c828
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-10-06 19:26:14 +0300

    in progress

---
+++ source/js/nav-toggle.js
@@ -0,0 +1,37 @@
+'use strict';
+
+(function (){
+  const header = document.querySelector('.js-header');
+  const headerButton = document.querySelector('.js-header-menu-button');
+  const page = document.querySelector('.js-page');
+
+  const headerOpenedClass = 'header--opened';
+  const headerClosedClass = 'header--closed';
+  const pageNavOpenedClass = 'page--nav-opened';
+
+  if(!page || !header || !headerButton) {
+    return false;
+  }
+
+  header.classList.add(headerClosedClass);
+
+  const open = function () {
+    header.classList.remove(headerClosedClass);
+    header.classList.add(headerOpenedClass);
+    page.classList.add(pageNavOpenedClass);
+  }
+
+  const close = function () {
+    header.classList.remove(headerOpenedClass);
+    header.classList.add(headerClosedClass);
+    page.classList.remove(pageNavOpenedClass);
+  }
+
+  headerButton.addEventListener('click', () => {
+    if(header.classList.contains(headerClosedClass)) {
+      open();
+    } else {
+      close();
+    }
+  })
+})();
