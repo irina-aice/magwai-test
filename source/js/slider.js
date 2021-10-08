80de34773d55af1d025c9784e0b5bc8ad0b718ed
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-10-08 16:30:29 +0300

    in progress

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function (){
+(function () {
   const slider = document.querySelector('.js-slider');
   const pagination = document.querySelector('.js-slider-pagination');
 
@@ -8,18 +8,42 @@
     return false;
   }
 
-  new Swiper(slider, {
-    //slidesPerView: 1,
+  let mySlider = new window.Swiper(slider, {
     pagination: {
       el: pagination,
       clickable: true,
       type: 'custom',
-      //renderCustom:	function(swiper, current, total) {},
+      renderCustom: function (swiper, current, total) {
+        let pagination = '';
+
+        for (let i = 0; i < swiper.slides.length; i++) {
+          const slide = swiper.slides[i];
+          let paginationTitle = slide.getAttribute('data-pagination');
+
+          let isActive = false;
+
+          if (swiper.activeIndex === i) {
+            isActive = true;
+          }
+
+          paginationTitle = `<button type="button" class="slider__bullet ${isActive ? 'slider__bullet--active' : ''}">${paginationTitle}</button>`;
+
+          pagination += paginationTitle;
+        }
+
+        return pagination;
+      },
     },
 
     autoplay: {
       delay: 5000,
     },
+  });
+
+  pagination.addEventListener('click', (evt) => {
+    const button = evt.target;
+    const index = Array.from(button.parentNode.children).indexOf(button);
+    mySlider.slideTo(index);
   });
 
 }());
